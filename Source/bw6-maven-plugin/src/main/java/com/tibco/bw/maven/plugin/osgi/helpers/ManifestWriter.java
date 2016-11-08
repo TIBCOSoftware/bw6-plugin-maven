4b810013adf3c369775c41ecd7aa89239fb5e8ed
Rajib Biswas <rbiswas@tibco.com>
2016-11-08 14:55:00 +0530

    New functionality: Manifest Version update with maven version PR: #22

--- Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/osgi/helpers/ManifestWriter.java
+++ Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/osgi/helpers/ManifestWriter.java
@@ -14,8 +14,8 @@ public class ManifestWriter {
 
     public static File updateManifest(MavenProject project , Manifest mf) throws IOException {
         Attributes attributes = mf.getMainAttributes();
-        if (attributes.getValue(Name.MANIFEST_VERSION) == null) {
-            attributes.put(Name.MANIFEST_VERSION, "1.0");
+        if((attributes.getValue(Name.MANIFEST_VERSION) == null || attributes.getValue(Name.MANIFEST_VERSION).equals("1.0")) && project.getVersion().equals("1.0.0-SNAPSHOT")) {
+            attributes.put(Name.MANIFEST_VERSION, project.getVersion());
         }
 
         File mfile = new File(project.getBuild().getDirectory(), "MANIFEST.MF");
