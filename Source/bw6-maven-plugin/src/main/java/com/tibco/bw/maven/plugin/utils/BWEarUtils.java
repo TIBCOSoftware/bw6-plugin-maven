5853ed76ebe097bfe765d0683b9bf495f0f289fd
Sonal Shingavi <sshingav@tibco.com>
2018-11-27 18:23:00 +0530

    Fix for #188

---
+++ Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/utils/BWEarUtils.java
@@ -0,0 +1,77 @@
+package com.tibco.bw.maven.plugin.utils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class BWEarUtils {
+
+	public static void ExtractEARFile(File earLocation, File EARFile)
+			throws IOException {
+		// getLog().debug("Extracting EAR File");
+		ZipInputStream zipIn = new ZipInputStream(new FileInputStream(EARFile));
+		ZipEntry entry = zipIn.getNextEntry();
+		while (entry != null) {
+			String filePath = earLocation.getAbsolutePath() + File.separator
+					+ entry.getName();
+			if (!entry.isDirectory()) {
+				ExtractEARFileEntry(zipIn, filePath);
+			} else {
+				File dir = new File(filePath);
+				dir.mkdir();
+			}
+			zipIn.closeEntry();
+			entry = zipIn.getNextEntry();
+		}
+		zipIn.close();
+	}
+
+	public static void ExtractEARFileEntry(ZipInputStream zipIn, String filePath)
+			throws IOException {
+		// getLog().debug("Extracting EAR File Entries ");
+		new File(new File(filePath).getParent()).mkdirs();
+		BufferedOutputStream bos = new BufferedOutputStream(
+				new FileOutputStream(filePath));
+		byte[] bytesIn = new byte[4096];
+		int read = 0;
+		while ((read = zipIn.read(bytesIn)) != -1) {
+			bos.write(bytesIn, 0, read);
+		}
+		bos.close();
+	}
+
+	public static void deleteSubFolder(File element) {
+		if (element.isDirectory()) {
+			for (File sub : element.listFiles()) {
+				deleteSubFolder(sub);
+			}
+		}
+		element.delete();
+	}
+
+	public static void DeleteEARFileEntries(File earLocation) {
+		// getLog().debug("Deleting EARFile Entries ");
+		String[] entries = earLocation.list();
+		for (String entry : entries) {
+			File currentFile = new File(earLocation.getPath(), entry);
+			// ************************
+
+			if (currentFile.getName().contains("META-INF")) {
+				deleteSubFolder(currentFile);
+			}
+			// **********************
+			if (currentFile.isDirectory()) {
+				DeleteEARFileEntries(currentFile);
+			}
+			if (!(currentFile.getName().contains(".ear")
+					|| currentFile.getName().contains("configFile") || currentFile
+					.getName().contains("pom"))) {
+				currentFile.delete();
+			}
+		}
+	}
+}
