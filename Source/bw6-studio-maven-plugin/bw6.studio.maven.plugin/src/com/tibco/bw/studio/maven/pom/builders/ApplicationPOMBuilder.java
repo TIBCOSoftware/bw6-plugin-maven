4807a70ed5f2b1307f5e56c6beff5d443b91034e
sdarekar <sdarekar@tibco.com>
2019-07-26 16:17:21 +0530

    Release version 2.3.0 updated

--- Source/bw6-studio-maven-plugin/bw6.studio.maven.plugin/src/com/tibco/bw/studio/maven/pom/builders/ApplicationPOMBuilder.java
+++ Source/bw6-studio-maven-plugin/bw6.studio.maven.plugin/src/com/tibco/bw/studio/maven/pom/builders/ApplicationPOMBuilder.java
@@ -319,7 +319,7 @@ public class ApplicationPOMBuilder extends AbstractPOMBuilder implements IPOMBui
 			ReportPlugin p = new ReportPlugin();
 			p.setGroupId("com.tibco.plugins");
 			p.setArtifactId("bw6-maven-plugin");
-			p.setVersion("2.2.0");
+			p.setVersion("2.3.0");
 			reporting.getPlugins().add(p);
 		}
 		
