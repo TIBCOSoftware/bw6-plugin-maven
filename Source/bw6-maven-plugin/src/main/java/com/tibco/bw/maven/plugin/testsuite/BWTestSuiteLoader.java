7c75ca7dd3406929e6e5f54b9e7a005ab19e1457
Sujata <sdarekar@tibco.com>
2020-06-09 18:09:57 +0530

    NPE occurs if Suite Name provided does not exist in the project

--- Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/testsuite/BWTestSuiteLoader.java
+++ Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/testsuite/BWTestSuiteLoader.java
@@ -1,6 +1,8 @@
 package com.tibco.bw.maven.plugin.testsuite;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +14,7 @@ import com.tibco.bw.maven.plugin.utils.BWFileUtils;
 
 public class BWTestSuiteLoader {
 
-	public List<File> collectTestCasesList(String baseDir){
+	public List<File> collectTestCasesList(String baseDir) throws IOException{
 		List<File> testSuitefile = new ArrayList<File>();
 		List<String> testSuiteNameList = new ArrayList<String>();
 		String[] testSuiteNames;
@@ -26,9 +28,13 @@ public class BWTestSuiteLoader {
 		Arrays.asList(testSuiteNames);
 		
 		String testFolderPath = BWFileUtils.getTestFolderName(baseDir.toString(),testSuiteNames[0]);
-		
-		for(String suiteName :testSuiteNames){
-			testSuiteNameList.add(testFolderPath.concat("//"+suiteName));
+		if(null != testFolderPath){
+			for(String suiteName :testSuiteNames){
+				testSuiteNameList.add(testFolderPath.concat("//"+suiteName));
+			}
+		}
+		else{
+			throw new FileNotFoundException("Test Suite file " +testSuiteNames[0]+ " is not found");
 		}
 		
 		BWTestConfig.INSTANCE.setTestSuiteNameList(Arrays.asList(testSuiteNames));
