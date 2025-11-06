6736964c2a8e6cd5296a0fd44a1184cc1021aa89
Shubham Deshmukh <167972232+sarundes-tibco@users.noreply.github.com>
2025-11-06 11:26:17 +0530

    BWCE-10373 added changes to make manifest and pom in sync for version capability

--- Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/osgi/helpers/ManifestParser.java
+++ Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/osgi/helpers/ManifestParser.java
@@ -5,9 +5,17 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+import java.util.jar.JarFile;
 import java.util.jar.JarInputStream;
 import java.util.jar.Manifest;
 
+import org.apache.maven.model.Dependency;
+
+import com.tibco.bw.maven.plugin.utils.BWFileUtils;
 import com.tibco.bw.maven.plugin.utils.Constants;
 
 public class ManifestParser {
@@ -61,7 +69,7 @@ public class ManifestParser {
 		String updatedProvidesCapabilities = ""; //$NON-NLS-1$
 		
 		Version versionObject = VersionParser.parseVersion(oldVersion);
-		String newVersion = versionObject.getMajor() + "." + versionObject.getMinor() + "." + versionObject.getMicro();
+		String newVersion = versionObject.getMajor() + "." + versionObject.getMinor() + "." + versionObject.getMicro()+"."+versionObject.getQualifier();
 		
 		if(manifest != null){
 			String capabilities = manifest.getMainAttributes().getValue(Constants.BUNDLE_PROVIDE_CAPABILITY);
@@ -95,5 +103,79 @@ public class ManifestParser {
 		}
 		
 		return updatedProvidesCapabilities;
+	}
+
+public static String getRequiredCapabilities(String reqCapbilitySource, List<Dependency> listDep) {
+		
+		String processedText = "";
+		String listModulesBw="";
+		for (Iterator<Dependency> iter = listDep.iterator(); iter.hasNext();) {
+			Dependency dep = iter.next();
+			
+			Path path = Paths.get(System.getProperty("user.home"), ".m2");
+			String fileName = dep.getArtifactId().concat("-" + dep.getVersion() + ".jar");
+			List<Path> result = null;
+			try {
+				result = BWFileUtils.findByFileName(path, fileName);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			File file = result.get(0).toFile();
+			Manifest mf = null;
+			try (JarFile jar = new JarFile(file)) {
+	             mf = jar.getManifest();
+	        } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+//			String newVersion = dep.getVersion();
+			String newVersion = mf.getMainAttributes().getValue(Constants.BUNDLE_VERSION);
+			if (newVersion != null && newVersion.contains(".qualifier")) {
+				String vers[] = newVersion.split(".qualifier");
+				newVersion = vers[0];
+			}
+			if (newVersion != null && newVersion.contains("-SNAPSHOT")) {
+				String vers[] = newVersion.split("-SNAPSHOT");
+				newVersion = vers[0];
+			}
+
+			String[] entries = reqCapbilitySource.split(",");
+			for (int i = 0; i < entries.length; i++) {
+				
+				String entry = entries[i];
+				String[] filters = entry.split(";");
+
+				if (filters[0].trim().equals("com.tibco.bw.module")) {
+					System.out.println("com.tibco.bw.module detected");
+					if (filters[1].trim().startsWith("filter:=\"(&(name="+ dep.getArtifactId() +")")) {
+
+						listModulesBw += filters[0] + ";" + "filter:=\"(&(name="+dep.getArtifactId()+")(version="+newVersion+"))\"" + ",";
+						
+					}
+				} 
+			}
+
+		}
+		if(reqCapbilitySource != null && !reqCapbilitySource.isEmpty() && reqCapbilitySource.contains(",")) {
+			String[] entriesOthers = reqCapbilitySource.split(",");
+			for (int i = 0; i < entriesOthers.length; i++) {
+				
+				String entry = entriesOthers[i];
+				String[] filters = entry.split(";");
+				String str = String.join(",", filters);
+	
+				if (!filters[0].trim().equals("com.tibco.bw.module")) {
+					processedText+= entry+",";
+				} 
+			}
+		}
+		processedText= listModulesBw+processedText;
+		processedText=processedText.replaceAll(",,",",");
+		if(processedText.endsWith(","))
+			processedText=processedText.substring(0, processedText.length() - 1);
+		return processedText;
 	}
 }
--- Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/osgi/helpers/ManifestWriter.java
+++ Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/osgi/helpers/ManifestWriter.java
@@ -4,15 +4,19 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 import java.util.jar.Attributes;
 import java.util.jar.Attributes.Name;
 import java.util.jar.Manifest;
 
+import org.apache.maven.model.Dependency;
 import org.apache.maven.project.MavenProject;
 
 import com.tibco.bw.maven.plugin.utils.BWProjectUtils;
 import com.tibco.bw.maven.plugin.utils.BWProjectUtils.MODULE;
 import com.tibco.bw.maven.plugin.utils.Constants;
+
 
 public class ManifestWriter {
 
@@ -49,8 +53,21 @@ public class ManifestWriter {
         //Updating provide capability for Shared Modules
         if(BWProjectUtils.getModuleType(mf) == MODULE.SHAREDMODULE){
         	String updatedProvide = ManifestParser.getUpdatedProvideCapabilities(mf, projectVersion);
-        	attributes.putValue(Constants.BUNDLE_PROVIDE_CAPABILITY, updatedProvide);
+        	if(updatedProvide!= null && !updatedProvide.isEmpty()) {
+        		attributes.putValue(Constants.BUNDLE_PROVIDE_CAPABILITY, updatedProvide);
+        	}
         }
+        
+//        if(BWProjectUtils.getModuleType(mf) == MODULE.APPMODULE) {
+        	List<Dependency> list=project.getDependencies();
+        	String updatedRequire=ManifestParser.getRequiredCapabilities(mf.getMainAttributes().getValue(Constants.BUNDLE_REQUIRE_CAPABILITY), list);
+        	System.out.print(updatedRequire);
+        	if(updatedRequire != null  &&  !updatedRequire.isEmpty()) {
+        		attributes.putValue(Constants.BUNDLE_REQUIRE_CAPABILITY, updatedRequire);
+        	}
+        	
+//        }
+        
 
     }
     
