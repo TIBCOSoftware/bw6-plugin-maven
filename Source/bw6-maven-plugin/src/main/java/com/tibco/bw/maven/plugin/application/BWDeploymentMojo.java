fdf6a8a05c991b4a593d71d4faa484724f495cd3
sarundes <sarundes@tibco.com>
2026-04-01 22:31:35 +0530

    BWCE-10380 reverting the changes done to remove oldbwdeploy goal

--- Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/application/BWDeploymentMojo.java
+++ Source/bw6-maven-plugin/src/main/java/com/tibco/bw/maven/plugin/application/BWDeploymentMojo.java
@@ -33,7 +33,7 @@ import com.tibco.bw.maven.plugin.tci.client.TCIDeployer;
 import com.tibco.bw.maven.plugin.utils.BWFileUtils;
 import com.tibco.bw.maven.plugin.utils.Constants;
 
-@Mojo(name = "bwdeployold")
+@Mojo(name = "bwdeployold", defaultPhase = LifecyclePhase.DEPLOY)
 public class BWDeploymentMojo extends AbstractMojo {
 	@Parameter(defaultValue="${session}", readonly=true)
 	private MavenSession session;
